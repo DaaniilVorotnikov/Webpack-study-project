191966e9131a0bb158834192d6b71f1cdd45c1f6
DaaniilVorotnikov <daavorotnikov@gmail.com>
2022-04-01 16:14:33 +0300

    webpack commit 3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import Post from './Post'
 import './styles/styles.css'
 import json from './assets/json.json'
-import WebpackLogo from './assets/webpack-logo.png'
-import xml from './assets/data.xml'
-import csv from './assets/data.csv'
+import WebpackLogo from '@assets/webpack-logo.png'
+import xml from '@assets/data.xml'
+import csv from '@assets/data.csv'
+import * as $ from 'jquery'
+//import './index.html'
+import './styles/less.less'
+import './styles/scss.scss'
 
 const post = new Post('Webpack Post Title', WebpackLogo)
+
+$('pre').html(post.toString())
+
 console.log('Post to String', post.toString())
 
 console.log('JSON', json)
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,48 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+const optimization = () => {
+    const config =  {
+        splitChunks:{
+            chunks: 'all'
+        }
+    }
+
+    if(isProd){
+        config.minimizer = [
+            new OptimizeCssAssetWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+
+ return config
+}
+
+const cssLoader = (extra) => {
+
+    const loaders = [
+        {loader: MiniCssExtractPlugin.loader},
+         'css-loader'
+        ]
+
+        if(extra){
+            loaders.push(extra)
+        }
+    
+    return loaders    
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+console.log('IS DEV', isDev)
+
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
@@ -9,20 +51,56 @@ module.exports = {
         analytics: './analytics.js'
     },
     output:{
-        filename: '[name].[contenthash].js',
+        filename: filename('js'),
         path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.json', '.png'],
+        alias: {
+            '@assets': path.resolve(__dirname, 'src/assets'),
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
+    optimization: optimization(),
+    devServer:{
+        static: {
+            directory: path.join(__dirname, 'src'),
+          },
+          compress: true,
+          port: 9000,
     },
     plugins: [
         new HTMLWebpackPlugin({
-            template: './index.html'
+            template: './index.html',
+            minify:{
+                collapseWhitespace: isProd
+            }
         }), 
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+            {
+                from: path.resolve(__dirname, 'src/favicon.ico'),
+                to: path.resolve(__dirname, 'dist')
+            }
+        ]}),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                use: cssLoader()
+            },
+            {
+                test: /\.less$/,
+                use: cssLoader('less-loader')
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: cssLoader('sass-loader')
             },
             {
                 test: /\.(png|jpg|svg|gif)$/,
